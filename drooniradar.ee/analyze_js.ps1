4214f06a797ff79ae699bcd092555ffed6cb5dcf
Andres Moks <andres.moks@gmail.com>
2026-01-22 13:07:17 +0200

    2

--- drooniradar.ee/analyze_js.ps1
+++
@@ -1,7 +0,0 @@
-$path = 'y:\Vasikas\dr1\drooniradar.ee\_app\immutable\nodes\4.CB2fylc3.js'
-$content = Get-Content $path -Raw
-Write-Host "--- SENSOR KEYWORDS ---"
-if ($content -match '.{0,200}sensor.{0,200}') { $matches[0] }
-if ($content -match '.{0,200}serialNr.{0,200}') { $matches[0] }
-if ($content -match '.{0,200}seen_by.{0,200}') { $matches[0] }
-if ($content -match '.{0,200}source.{0,200}') { $matches[0] }
